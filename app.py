38cedcdda8832af382d262cb365de0889a6c104e
Ana María <120389092+AnaM8129@users.noreply.github.com>
2026-02-26 13:21:51 -0500

    Update app.py

--- app.py
+++ app.py
@@ -3,3 +3,4 @@ st.title("Mi primera app")
 st.header("Esta es mi página de presentación")
 from PIL import Image
 image = Image.open("tarot.jpg")
+st.image(image, caption="Esta es una imagen del tarot")
